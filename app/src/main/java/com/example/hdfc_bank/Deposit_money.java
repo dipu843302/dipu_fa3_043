b4596471f93229a4af65ab69b8db2625b7c51829
dipu843302 <dipu843302@gmail.com>
2021-08-09 15:23:20 +0530

    add

--- app/src/main/java/com/example/hdfc_bank/Deposit_money.java
+++ app/src/main/java/com/example/hdfc_bank/Deposit_money.java
@@ -8,18 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Create_account extends AppCompatActivity {
-private Button submit;
+public class Deposit_money extends AppCompatActivity {
+private Button proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
-        submit=findViewById(R.id.creat_submit);
-        submit.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_deposit_money);
+        proceed=findViewById(R.id.proceed);
+        proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Create_account.this,MainActivity2.class);
-                Toast.makeText(getApplicationContext(),"Account has created",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(Deposit_money.this,MainActivity2.class);
+                Toast.makeText(getApplicationContext(),"Transaction Successfully",Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
         });
